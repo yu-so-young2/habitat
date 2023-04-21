dcee12061fc6c8d8ebfc915ab53d541c6b990a76
fftl <fftl0785@gmail.com>
2023-04-21 16:30:07 +0900

    Feat : spring project init
    스프링 프로젝트 시작
    
    Related to : #S08P31A704-95

---
+++ back/habitat/src/test/java/com/ssafy/habitat/HabitatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.habitat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
