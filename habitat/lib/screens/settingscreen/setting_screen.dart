5c4f2c78128b3facebb0a3596b7e02b4625209df
박병우 <bottler3123@naver.com>
2023-04-28 13:43:54 +0900

    Fix : android emul에 맞게 크기 조절
    안드로이드 에뮬레이터에 맞게 크기를 조절하고 스크롤 추가
    
    Related to : #S08P31A704-239

--- habitat/lib/screens/settingscreen/setting_screen.dart
+++ habitat/lib/screens/settingscreen/setting_screen.dart
@@ -12,7 +12,7 @@ class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown.shade50,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
           "Setting 설정",
@@ -34,104 +34,71 @@ class _SettingScreenState extends State<SettingScreen> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
+      body: ListView(
         children: [
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 100, vertical: 50),
-            height: 100,
-            // width: 200,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: const BorderRadius.all(
-                Radius.circular(12),
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.7),
-                  blurRadius: 5.0,
-                  spreadRadius: 0.0,
-                  offset: const Offset(0, 0),
-                )
-              ],
-            ),
-            child: const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(right: 40),
-                  child: Icon(
-                    Icons.local_florist_rounded,
-                    size: 68,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                height: 100,
+                // width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(12),
                   ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 5.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(0, 0),
+                    )
+                  ],
                 ),
-                Column(
+                child: const Row(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      "쏘영쏘",
-                      style: TextStyle(
-                        fontSize: 36,
-                        fontWeight: FontWeight.w600,
+                    Padding(
+                      padding: EdgeInsets.only(right: 40),
+                      child: Icon(
+                        Icons.local_florist_rounded,
+                        size: 68,
                       ),
                     ),
-                    Text(
-                      "목표 음수량 : 1.5L",
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w600,
-                      ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "쏘영쏘",
+                          style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        Text(
+                          "목표 음수량 : 1.5L",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              const settingbox(),
+              const settingbox(),
+              const settingbox(),
+              const settingbox(),
+              const settingbox(),
+              // Bluetooth(),
+            ],
           ),
-          Container(
-            margin: const EdgeInsets.only(bottom: 20),
-            padding: const EdgeInsets.symmetric(horizontal: 100),
-            alignment: Alignment.bottomLeft,
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "목표설정",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextButton(
-                  onPressed: null,
-                  child: Text(
-                    "목표 음수량 설정",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: null,
-                  child: Text(
-                    "목표 음수량 추천받기",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const settingbox(),
-          const settingbox(),
-          const settingbox(),
-          // Bluetooth(),
         ],
       ),
       bottomNavigationBar: const DockBar(),
@@ -147,25 +114,29 @@ class settingbox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.only(bottom: 20),
-      padding: const EdgeInsets.symmetric(horizontal: 100),
+      margin: const EdgeInsets.only(left: 50, bottom: 20),
       alignment: Alignment.bottomLeft,
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             "목표설정",
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.w600,
             ),
           ),
-          SizedBox(
-            height: 10,
+          const SizedBox(
+            height: 6,
           ),
           TextButton(
             onPressed: null,
-            child: Text(
+            style: TextButton.styleFrom(
+              // minimumSize: Size.zero,
+              padding: const EdgeInsets.only(left: 10),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            child: const Text(
               "목표 음수량 설정",
               style: TextStyle(
                 fontSize: 18,
@@ -175,7 +146,12 @@ class settingbox extends StatelessWidget {
           ),
           TextButton(
             onPressed: null,
-            child: Text(
+            style: TextButton.styleFrom(
+              // minimumSize: Size.zero,
+              padding: const EdgeInsets.only(left: 10),
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+            ),
+            child: const Text(
               "목표 음수량 추천받기",
               style: TextStyle(
                 fontSize: 18,
