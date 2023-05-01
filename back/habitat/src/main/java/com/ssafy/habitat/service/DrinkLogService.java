5a7fda6ae2e06b03f9d35b7298db01b9ebf23f00
fftl <fftl0785@gmail.com>
2023-05-01 15:02:00 +0900

    Feat : drinkLog API 작업
    getDailyDrink API 생성
    
    Related to : #S08P31A704-264

--- back/habitat/src/main/java/com/ssafy/habitat/service/DrinkLogService.java
+++ back/habitat/src/main/java/com/ssafy/habitat/service/DrinkLogService.java
@@ -2,11 +2,15 @@ package com.ssafy.habitat.service;
 
 import com.ssafy.habitat.entity.DrinkLog;
 import com.ssafy.habitat.entity.User;
+import com.ssafy.habitat.exception.CustomException;
 import com.ssafy.habitat.repository.DrinkLogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -26,6 +30,14 @@ public class DrinkLogService {
 
     public List<DrinkLog> getAllLogs(User user) {
         List<DrinkLog> drinkLogList = drinkLogRepository.findAllByUserAndIsRemoved(user, false);
+        return drinkLogList;
+    }
+
+    public List<DrinkLog> getDailyLogs(User user){
+        LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(0,0,0)); //오늘 00:00:00
+        LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59)); //오늘 23:59:59
+        List<DrinkLog> drinkLogList = drinkLogRepository.findAllByUserAndModifiedAtBetween(user, startDatetime, endDatetime);
+
         return drinkLogList;
     }
 
