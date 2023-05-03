f393c00f69a91347309969620ee86cef07d77fa9
심정윤 <shimmumu@naver.com>
2023-05-03 17:49:02 +0900

    Fix : Combine.ino
    아직 미완(압력센서 제어 코드 제외하고 틀 구상)

--- Combine/Combine.ino
+++ Combine/Combine.ino
@@ -1,3 +1,15 @@
+////---------NeoPixel---------////
+#include <Adafruit_NeoPixel.h>
+
+//고정적으로 사용할 내용을 미리 선언
+#define LED_PIN 13     //네오픽셀에 신호를 줄 핀번호
+#define LED_COUNT 10  //아두이노에 연결된 네오픽셀의 개수
+
+////---------압력센서---------////
+int before_drink=0;
+int now_drink=0;
+int total_drink=0;
+
 ////---------타이머---------////
 #include "esp_system.h"
 
@@ -22,7 +34,13 @@ String strValue = "";
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
+bool write_drink_SPIFFS = false;
 int cnt = 0;
+String receive_label = "";
+String receive_data = "";
+int goal = 0;
+int alarm = 0;
+String account = ""; // 계정
 
 BLEServer *pServer = NULL;
 BLEService *pService = NULL;
@@ -42,13 +60,27 @@ class MyCallbackHandler : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) {
     // 수신된 데이터 받아오기
     std::string value = pCharacteristic->getValue();
-   
+    int isvalue=0;
     if (value.length() > 0) {
       Serial.print("Receive");
       Serial.print("Received value: ");
       for (int i = 0; i < value.length(); i++) {
         Serial.print(value[i]);
+
+        // lable : data
+        if(value[i]==":")   isvalue=1;
+        
+        if(isvalue==0)
+          receive_label = receive_label + (char)value[i];
+        else if(isvalue==1)
+          receive_data = receive_data + (int)value[i];
       }
+
+      if(receive_label=="goal")
+        goal = receive_data;
+      if(receive_label=="alarm")
+        alarm = receive_data;
+      
       Serial.println();
     }
   }
@@ -56,15 +88,24 @@ class MyCallbackHandler : public BLECharacteristicCallbacks {
 
 void setup() {
   Serial.begin(115200);
+
+  ////---------NeoPixel---------////
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // 네오픽셀에 빛을 출력하기 위한 것인데 여기서는 모든 네오픽셀을 OFF하기 위해서 사용한다.
+  strip.setBrightness(50); // 네오픽셀의 밝기 설정(최대 255까지 가능)
+  
   ////---------SPIFFS---------////
   Serial.println();
   if (!SPIFFS.begin()) {
     Serial.println("Failed to mount file system");
     return;
   }
+
   SPIFFS.format();
   listDir("/"); 
   writeFile("/hello.txt", "");
+
+
 
   ////---------블루투스---------////
   Serial.println("Starting BLE...");
@@ -109,6 +150,7 @@ void setup() {
   Serial.println("BLE server is ready.");
 
 
+
   ////---------타이머---------////
   Serial.println("running setup");
   timer = timerBegin(0, 80, true);                  //timer 0, div 80
@@ -121,45 +163,42 @@ void setup() {
 }
 
 void loop() {
+  ////--------압력센서--------////
+  // 센서값 받아오면 now_drink에 저장
+  // now_drink = ???;
 
-  ////---------타이머---------////
-  timerWrite(timer, 0); //reset timer (feed watchdog)
-  if((millis()/1000) % 60 == 0)
-  {
-    Min++;
-    Serial.print("Min = ");
-    Serial.println(Min);
+  if(goal == total_drink) theaterChaseRainbow(50);
+  // 게이지 상승 
+  if(goal*0.1 == total_drink)    theaterChaseRainbow(1);
+  else if(goal*0.2 == total_drink)    theaterChaseRainbow(2);
+  else if(goal*0.3 == total_drink)    theaterChaseRainbow(3);
+  else if(goal*0.4 == total_drink)    theaterChaseRainbow(4);
+  else if(goal*0.5 == total_drink)    theaterChaseRainbow(5);
+  else if(goal*0.6 == total_drink)    theaterChaseRainbow(6);
+  else if(goal*0.7 == total_drink)    theaterChaseRainbow(7);
+  else if(goal*0.8 == total_drink)    theaterChaseRainbow(8);
+  else if(goal*0.9 == total_drink)    theaterChaseRainbow(9);
 
-    // 파일 생성 및 데이터 쓰기
-    String data1 = "min : ";
-    String data2 =  (String)Min;
-    String data3 =  "\r\n";
-    String data = data1 + data2 + data3;
-    const char* Data = data.c_str();
+  
 
-    appendFile("/hello.txt", Data);
-    readFile("/hello.txt");  
-    
-  }
-  if(Min % 60 == 0 && Min != 0)
-  {
-    Min = 0;
-    Hour++;
-    Serial.print("Hour = ");
-    Serial.println(Hour); 
-  }
 
-  Serial.print("Time to = ");
-  Serial.println(millis()/1000); 
+
+
+
+
+
 
   ////---------블루투스---------////
   // 연결이 잘 되고 있는 상황
   // 데이터 갱신 후 notification
   if(deviceConnected){	// notify changed value
-    pCharacteristic->setValue((uint8_t*)strValue.c_str(), strValue.length());
-    pCharacteristic->notify();
-    cnt++;
-    delay(3);   // client가 올바르게 정보를 수산할 수 있도록 여유의 시간(레퍼런스에서 3ms)  
+    if(before_drink != now_drink){
+      pCharacteristic->setValue((uint8_t*)strValue.c_str(), strValue.length());
+      pCharacteristic->notify();
+      cnt++;
+      delay(3);   // client가 올바르게 정보를 수산할 수 있도록 여유의 시간(레퍼런스에서 3ms)  
+      before_drink = now_drink;
+    }
   }
   // 이전에 연결한 기록이 있는 상태에서 견결이 끊기 상황
   if (!deviceConnected && oldDeviceConnected) {	// disconnecting
@@ -167,16 +206,72 @@ void loop() {
     pServer->startAdvertising(); // restart advertising
     Serial.println("start advertising");
     oldDeviceConnected = deviceConnected;
+    write_drink_SPIFFS = true;
+    // 끊김 확인한 후에 메모리 초기화
+    SPIFFS.format();
   }
   // 연결은 되었지만 이전에 연결한 기록이 없는 상황
+  // 연결된 기기에 데이터 전송
   if (deviceConnected && !oldDeviceConnected) {	// connecting
     oldDeviceConnected = deviceConnected;
+    write_drink_SPIFFS = false;
+    pCharacteristic->setValue((uint8_t*)strValue.c_str(), strValue.length());
+    pCharacteristic->notify();
+    delay(3);
+  }
+  // 끊김 확인 이후
+  if(!deviceConnected && write_drink_SPIFFS){
+    ////---------타이머---------////
+    String data1 = "";
+    String data2 =  "";
+    String data3 =  "";
+    String data = "";
+    timerWrite(timer, 0); //reset timer (feed watchdog)
+    if((millis()/1000) % 60 == 0)
+    {
+      Min++;
+      Serial.print("Min = ");
+      Serial.println(Min);
+
+      // 파일 생성 및 데이터 쓰기
+      data1 = "min : ";
+      data2 =  (String)Min;
+      data3 =  "\r\n";
+      data = data1 + data2 + data3;
+      const char* Data = data.c_str();
+
+    }
+    if(Min % 60 == 0 && Min != 0)
+    {
+      Min = 0;
+      Hour++;
+      Serial.print("Hour = ");
+      Serial.println(Hour); 
+    }
+
+    Serial.print("Time to = ");
+    Serial.println(millis()/1000); 
+    
+    if(before_drink != now_drink){
+      String st = ":";
+      String drink_st = (String)now_drink;
+      String d = Data + st + drink_st;
+
+      appendFile("/hello.txt", d);
+
+      before_drink = now_drink;
+    }
+    
   }
 
   delay(1000); //simulate work
   
 }
 
+
+
+
+////---------SPIFFS---------////
 
 void listDir(const char * dirname){
   Serial.printf("Listing directory: %s\r\n", dirname);
@@ -200,7 +295,7 @@ void readFile(const char * path){
     Serial.println("- failed to open file for reading");
     return;
   }
-
+  
   strValue="";
   Serial.println("read from file:");
   while(file.available()){
@@ -256,5 +351,27 @@ void deleteFile(const char * path){
     Serial.println("file deleted");
   } else {
     Serial.println("delete failed");
+  }
+}
+
+
+
+
+////---------NeoPixel--------////
+// NeoPixel에 달린 LED를 각각 다른색으로 시작하여 다양한색으로 5번 반복한다
+void theaterChaseRainbow(int wait) {
+  int firstPixelHue = 0;
+  for (int a = 0; a < 5; a++) { 
+    for (int b = 0; b < 3; b++) {
+      strip.clear();         
+      for (int c = b; c < strip.numPixels(); c += 3) {        
+        int hue = firstPixelHue + c * 65536L / strip.numPixels();
+        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
+        strip.setPixelColor(c, color);
+      }
+      strip.show();                
+      delay(wait);                 
+      firstPixelHue += 65536 / 90; 
+    }
   }
 }
