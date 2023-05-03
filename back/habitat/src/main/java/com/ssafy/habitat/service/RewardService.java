a4af1e7cf03a588a8424456e6623633b4837e916
soyoung <sy980911@gmail.com>
2023-05-03 17:25:53 +0900

    Feat : 스트릭 조건 달성 해금 구현
    유저 스트릭 증가, 꽃 해금 구현
    
    Related to : #S08P31A704-285

--- back/habitat/src/main/java/com/ssafy/habitat/service/RewardService.java
+++ back/habitat/src/main/java/com/ssafy/habitat/service/RewardService.java
@@ -51,13 +51,8 @@ public class RewardService {
 
                 // 다른 조건들까지 모두 해금되었다면
                 if(isFullyUnlocked(userFlower)) {
-                    // 완전 해금
-                    userFlower.setUnlocked(true);
-                    userFlowerService.addUserFlower(userFlower);
-
-                    // 완전 해금 기록 저장
-                    newUserFlowerLog = UserFlowerLog.builder().user(user).flower(flower).mission('t').build();
-                    userFlowerLogService.addUserFlowerLog(newUserFlowerLog);
+                    // 유저의 해당 꽃 완전 해금
+                    unlockUserFlower(user, flower, userFlower);
 
                     // 웹소켓 전송
                     System.out.println(user.getNickname()+" 님 친구 수 " + friendCnt+"명 달성으로 " + flower.getName() + " 꽃이 해금되었습니다!!!");
@@ -88,13 +83,8 @@ public class RewardService {
 
                 // 다른 조건들까지 모두 해금되었다면
                 if(isFullyUnlocked(userFlower)) {
-                    // 완전 해금
-                    userFlower.setUnlocked(true);
-                    userFlowerService.addUserFlower(userFlower);
-
-                    // 완전 해금 기록 저장
-                    newUserFlowerLog = UserFlowerLog.builder().user(user).flower(flower).mission('t').build();
-                    userFlowerLogService.addUserFlowerLog(newUserFlowerLog);
+                    // 유저의 해당 꽃 완전 해금
+                    unlockUserFlower(user, flower, userFlower);
 
                     // 웹소켓 전송
                     System.out.println(user.getNickname()+" 님 누적음수량 "+ flower.getDrinkValue()+"ml 달성으로 " + flower.getName() + " 꽃이 해금되었습니다!!!");
@@ -104,11 +94,69 @@ public class RewardService {
 
     }
 
-    public boolean isFullyUnlocked(UserFlower userFlower) {
+    public void checkStreakUnlock(User user) {
+        // 현재 스트릭에 따른 달성여부 확인
+
+        int curStreak = streakLogService.getDailyStreakLog(user).getCurStreak(); // 스트릭 변화에 따른 현재 스트릭
+        List<UserFlower> userFlowerList = userFlowerService.getLockedFlowerList(user); // 해당 유저의 해금안된 꽃 리스트
+
+        for (int i = 0; i < userFlowerList.size(); i++) { // 아직 해금 되지 않은 꽃들에 대하여
+            UserFlower userFlower = userFlowerList.get(i);
+            Flower flower = userFlower.getFlower();
+
+            // 이전에는 스트릭 조건을 달성하지 못했었고, 현재 스트릭 조건을 달성했다면
+            if(userFlower.isStreak() == false && curStreak >= flower.getStreakValue()) {
+                // 해당 꽃의 스트릭 조건 해금
+                userFlower.setStreak(true);
+                userFlowerService.addUserFlower(userFlower);
+
+                // 해금 기록 저장
+                UserFlowerLog newUserFlowerLog = UserFlowerLog.builder().user(user).flower(flower).mission('s').build();
+                userFlowerLogService.addUserFlowerLog(newUserFlowerLog);
+
+                // 다른 조건들까지 모두 해금되었다면
+                if(isFullyUnlocked(userFlower)) {
+                    // 유저의 해당 꽃 완전 해금
+                    unlockUserFlower(user, flower, userFlower);
+
+                    // 웹소켓 전송
+                    System.out.println(user.getNickname()+" 님 연속스트릭 "+ flower.getDrinkValue()+"일 달성으로 " + flower.getName() + " 꽃이 해금되었습니다!!!");
+                }
+            }
+        }
+    }
+
+
+    public boolean checkStreakSuccess(User user) {
+        int totalDailyDrink = drinkLogService.getDailyTotalDrink(user); // 유저의 오늘 누적음수량 조회
+        StreakLog todayStreakLog = streakLogService.getDailyStreakLog(user); // 유저의 오늘 스트릭 가져오기
+
+        // 아직 오늘의 스트릭이 생성된 적 없고 목표를 달성한 경우
+        if(todayStreakLog == null && totalDailyDrink >= user.getGoal()) {
+            // 오늘의 목표 달성 웹소켓 전송
+
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isFullyUnlocked(UserFlower userFlower) {
         // 모든 조건을 달성했다면 true 리턴
         if(userFlower.isFriend() && userFlower.isDrink() && userFlower.isStreak() && userFlower.isConnect()) {
             return true;
         }
         return false;
     }
+
+    private void unlockUserFlower(User user, Flower flower, UserFlower userFlower) {
+        // 유저의 해당 꽃 완전 해금
+        userFlower.setUnlocked(true);
+        userFlowerService.addUserFlower(userFlower);
+
+        // 완전 해금 기록 저장
+        UserFlowerLog newUserFlowerLog = UserFlowerLog.builder().user(user).flower(flower).mission('t').build();
+        userFlowerLogService.addUserFlowerLog(newUserFlowerLog);
+    }
+
+
 }
