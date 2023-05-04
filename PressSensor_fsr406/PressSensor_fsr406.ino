dfe6045b896c8e23835ad11eec0f5c6a43c960cc
심정윤 <shimmumu@naver.com>
2023-05-04 16:39:52 +0900

    Feat : PressSensor_fsr406.ino
    압력센서 센신값 가져오기

---
+++ PressSensor_fsr406/PressSensor_fsr406.ino
@@ -0,0 +1,13 @@
+const int pressSensor = 26;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int value = analogRead(pressSensor);
+  Serial.println(value);
+  delay(1000);
+}
