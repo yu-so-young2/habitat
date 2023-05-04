071b93e72adb777d295e504aa64c12ce12a8ccea
YounghyunS2 <109256160+YounghyunS2@users.noreply.github.com>
2023-05-04 21:27:46 +0900

    Fix : social friend request

--- habitat/lib/api/friends/api_friendrequest.dart
+++ habitat/lib/api/friends/api_friendrequest.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:habitat/api/base_url.dart';
 import 'package:habitat/models/friend_request_model.dart';
 import 'package:http/http.dart' as http;
@@ -20,5 +21,21 @@ class ApiFriendRequestList {
     }
 
     return friendlist;
+  }
+}
+
+class ApiFriendRequest {
+  final String baseurl = BaseUrl().rooturl;
+  final int friendRequestKey = 0;
+  bool result = false;
+
+  void postFriendRequest(int requestCode, String id) async {
+    Uri url = Uri.http(baseurl, 'friends/request/ok', {'userKey': id});
+    http.Response response = await http.post(url,
+        body: jsonEncode({'friendRequestKey': requestCode}));
+
+    if (response.statusCode == 200) {
+      debugPrint('수락');
+    }
   }
 }
