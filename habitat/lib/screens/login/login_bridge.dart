62cd22d9715995cc7b7d8facd2d3da1742977728
MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
2023-05-08 13:11:32 +0900

    Squashed commit of the following:
    commit 2b47e432361aa02e4ef37bca123f827ef669c2f5
    Author: MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
    Date:   Mon May 8 13:07:30 2023 +0900
    
        Feat : Google Sign in, multidex
    
        Related to : [#S08P31A704-380]
    
    commit 550647d7c016b5b78949037a9b3be80fe2a9668b
    Author: MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
    Date:   Mon May 1 17:40:27 2023 +0900
    
        Feat : Android 구성 파일
    
    commit 5e5439c106de95c7a90357348131f9832e747a48
    Author: MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
    Date:   Mon May 1 17:07:55 2023 +0900
    
        Fix : package name
    
    commit 1f17065d8de905342c46f1fc463e9e068b602589
    Author: MingyuGhim <109265235+MingyuGhim@users.noreply.github.com>
    Date:   Mon May 1 15:59:03 2023 +0900
    
        Feat : Firebase 연동

---
+++ habitat/lib/screens/login/login_bridge.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:habitat/screens/login/login_screen.dart';
+import 'package:habitat/screens/report_screen.dart';
+
+class LoginBridge extends StatelessWidget {
+  const LoginBridge({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
+          if (!snapshot.hasData) {
+            return const LoginScreen();
+          } else {
+            return const ReportScreen();
+            // return Center(
+            //   child: Column(children: [
+            //     Text("${snapshot.data?.displayName ?? 'anon'}님 환영합니다."),
+            //   ]),
+            // );
+          }
+        },
+      ),
+    );
+  }
+}
