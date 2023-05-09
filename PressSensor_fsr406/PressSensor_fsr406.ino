6fea4a738b80c36ca85c637502b55341c0085731
심정윤 <shimmumu@naver.com>
2023-05-09 15:57:35 +0900

    Fix : PressSensor_fsr406.ino
    압력센서 데이터 리터로 변경하는 함수만 생성

--- PressSensor_fsr406/PressSensor_fsr406.ino
+++ PressSensor_fsr406/PressSensor_fsr406.ino
@@ -1,5 +1,11 @@
 const int pressSensor = 26;
 
+int Goal = 1000;
+int init_drink;
+int before_drink = -1;
+int now_drink;
+int total_drink;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -8,6 +14,16 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   int value = analogRead(pressSensor);
+
+  changeLiter(presssensor_value);
   Serial.println(value);
   delay(1000);
+}
+
+
+void changeLiter(int drink){
+  now_drink = init_drink - drink;
+
+  /// 값 변경
+
 }
