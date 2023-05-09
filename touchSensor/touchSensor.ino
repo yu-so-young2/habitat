3399e57fec90f4707ed3a03f482bf681fa99841d
심정윤 <shimmumu@naver.com>
2023-05-09 15:53:12 +0900

    Fix : touchSensor.ino
    터치센서 기본 코드

---
+++ touchSensor/touchSensor.ino
@@ -0,0 +1,25 @@
+const int touchsensor = 14;      // 터치센서 SIG를 14번 핀에 연결
+int state = 0;                  // 현재 상태 변수 선언
+
+int cnt=0;
+void setup() {
+
+  Serial.begin (115200);        // 시리얼 모니터 통신 설정
+  pinMode (touchsensor, INPUT);     // 터치센서 신호값을 입력으로 설정 
+
+}
+
+void loop() {
+  state = digitalRead (touchsensor); 
+  if(state == HIGH)
+  {
+    cnt++;
+  }
+  if(cnt>=2)
+  {
+    Serial.println("w");
+    cnt=0;
+  }
+
+  delay(1000);
+}
