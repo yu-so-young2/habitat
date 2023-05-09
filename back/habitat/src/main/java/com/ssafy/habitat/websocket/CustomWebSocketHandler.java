2387e2217875a61c0b56c3e3dd9c35d61f75b19b
soyoung <sy980911@gmail.com>
2023-05-09 14:10:56 +0900

    Feat : WebSocket 빌드 수정

---
+++ back/habitat/src/main/java/com/ssafy/habitat/websocket/CustomWebSocketHandler.java
@@ -0,0 +1,61 @@
+package com.ssafy.habitat.websocket;
+
+import com.ssafy.habitat.entity.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.*;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+public class CustomWebSocketHandler implements WebSocketHandler {
+
+    private static final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        // 클라이언트가 연결되었을 때 호출됩니다.
+        // 클라이언트로부터 userKey를 추출하여 매핑합니다.
+
+        // path: /websocket/{userKey}
+        String userKey = session.getUri().getPath().split("/")[2];
+        sessions.put(userKey, session);
+
+    }
+
+    @Override
+    public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        // 전송 오류가 발생했을 때 호출됩니다.
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+        // 클라이언트가 연결을 종료했을 때 호출됩니다.
+        // 매핑된 세션을 제거합니다.
+        String userKey = session.getUri().getPath().split("/")[2];
+        sessions.remove(userKey);
+    }
+
+    @Override
+    public boolean supportsPartialMessages() {
+        return false;
+    }
+
+    // userKey를 이용하여 매핑된 세션을 반환하는 메소드입니다.
+    public WebSocketSession getSession(String userKey) {
+        return sessions.get(userKey);
+    }
+
+    // user의 userKey를 추출하여 해당 유저의 세션에 message 웹소켓을 전송합니다.
+    public void sendMessage(User user, String message) throws IOException {
+        WebSocketSession session = getSession(user.getUserKey());
+        if(session != null) {
+            session.sendMessage(new TextMessage(message));
+        }
+    }
+}
--- back/habitat/src/main/java/com/ssafy/habitat/websocket/WebSocketConfig.java
+++ back/habitat/src/main/java/com/ssafy/habitat/websocket/WebSocketConfig.java
@@ -1,13 +1,22 @@
-//package com.ssafy.habitat.websocket;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.socket.server.standard.ServerEndpointExporter;
-//
-//@Component
-//public class WebSocketConfig {
-//    @Bean
-//    public ServerEndpointExporter serverEndpointExporter() {
-//        return new ServerEndpointExporter();
-//    }
-//}
+package com.ssafy.habitat.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Autowired
+    private CustomWebSocketHandler customWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(customWebSocketHandler, "/websocket/{userKey}")
+                .setAllowedOrigins("*");
+    }
+
+}
