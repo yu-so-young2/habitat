e69eaeaa771a4da0b1142d5893ae0ce95f97f446
박병우 <bottler3123@naver.com>
2023-05-11 07:58:21 +0900

    Feat : 로그인 getx 처리중

--- habitat/lib/screens/login/login_bridge.dart
+++ habitat/lib/screens/login/login_bridge.dart
@@ -1,20 +1,46 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:habitat/screens/login/login_screen.dart';
-import 'package:habitat/screens/report_screen.dart';
+import 'package:habitat/screens/main/main_screen.dart';
 
-class LoginBridge extends StatelessWidget {
+class LoginBridge extends StatefulWidget {
   const LoginBridge({Key? key}) : super(key: key);
 
   @override
+  State<LoginBridge> createState() => _LoginBridgeState();
+}
+
+class _LoginBridgeState extends State<LoginBridge> {
+  static const storage = FlutterSecureStorage();
+
+  String readOnboarding = '';
+  String userInfo = '';
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      asyncMethod();
+    });
+  }
+
+  asyncMethod() async {
+    //read 함수를 통하여 key값에 맞는 정보를 불러온다. 불러오는 결과의 타입은 String
+    readOnboarding = await storage.read(key: "onboarding") ?? 'none';
+    userInfo = await storage.read(key: "userInfo") ?? 'none';
+    debugPrint(readOnboarding);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-        if (!snapshot.hasData) {
+    return Builder(
+      builder: (BuildContext context) {
+        if (readOnboarding == 'none') {
+          return const MainScreen();
+        } else if (userInfo == 'none') {
           return const LoginScreen();
         } else {
-          return const ReportScreen();
+          return const MainScreen();
           // return Center(
           //   child: Column(children: [
           //     Text("${snapshot.data?.displayName ?? 'anon'}님 환영합니다."),
