64e87cf130afadcf26126ddf069787f8a69992fb
박병우 <bottler3123@naver.com>
2023-05-12 01:38:01 +0900

    Feat : 온보딩 슬라이드 페이지 제작

--- habitat/lib/screens/onboarding/onboarding_screen.dart
+++ habitat/lib/screens/onboarding/onboarding_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:habitat/screens/login/login_screen.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -13,45 +14,131 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   static const storage = FlutterSecureStorage();
   dynamic check = "";
 
-  checkOnboarding() async {
-    var check = await storage.read(key: 'onboarding');
-  }
-
   readOnboarding() async {
     await storage.write(key: 'onboarding', value: 'onboarding');
   }
 
+  final PageController pageController = PageController(
+    initialPage: 0,
+  );
+
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      checkOnboarding();
-      debugPrint("온보딩 체크 $check");
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          const Text('온보딩 스크린'),
-          Center(
-            child: TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: Colors.white,
+          PageView(
+            controller: pageController,
+            children: [
+              // 첫 번째 페이지
+              SizedBox.expand(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          color: Colors.red,
+                          height: 400,
+                        ),
+                        Image.asset('lib/assets/images/sunflower.png')
+                      ],
+                    ),
+                    const Text(
+                      'Page index : 0',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {
-                readOnboarding();
-                debugPrint("넘어가기 전 체크 $check");
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => LoginScreen()),
-                    (route) => false);
+              // 두 번째 페이지
+              SizedBox.expand(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          color: Colors.yellow,
+                          height: 400,
+                        ),
+                        Image.asset('lib/assets/images/sunflower.png')
+                      ],
+                    ),
+                    const Text(
+                      'Page index : 1',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+              // 세 번째 페이지
+              SizedBox.expand(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Container(
+                          color: Colors.green,
+                          height: 400,
+                        ),
+                        Image.asset('lib/assets/images/sunflower.png')
+                      ],
+                    ),
+                    const Text(
+                      'Page index : 2',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+              ),
+              // 네 번째 페이지
+              SizedBox.expand(
+                child: Center(
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      readOnboarding();
+                      debugPrint("넘어가기 전 체크 $check");
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) => LoginScreen()),
+                          (route) => false);
+                    },
+                    child: const Text("시작하기"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 30,
+            child: SmoothPageIndicator(
+              controller: pageController, // PageController
+              count: 4,
+              effect: const ScaleEffect(
+                activeDotColor: Colors.lightBlue,
+              ), // your preferred effect
+              onDotClicked: (index) {
+                pageController.animateToPage(
+                  index,
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.ease,
+                );
               },
-              child: const Text("시작"),
             ),
           )
         ],
